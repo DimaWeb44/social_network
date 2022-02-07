d6d4ab50c3ca3bea98e196d8ba6bce3ad75fe664
DimaWeb44 <dima.web44@gmail.com>
2022-02-07 22:26:40 +0300

    lesson 51

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import DialogsContainer from './components/Dialogs/DialogsConainer';
 import UsersContainer from './components/Users/UsersContainer';
 
@@ -15,6 +15,7 @@ function App() {
             <Navbar/>
             <div className="app-wrapper-content">
                 <Routes>
+                    <Route path={'/'} element={<Navigate to="/profile/*"/>}/>
                     <Route path="/profile/*" element={<Profile/>}/>
                     <Route path="/dialogs/*" element={<DialogsContainer/>}/>
                     <Route path="/users/*" element={<UsersContainer/>}/>
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,52 +1,23 @@
+import axios from 'axios';
 import React from 'react';
 import {v1} from 'uuid';
 import s from './Users.module.css'
-import { UsersPropsType } from './UsersContainer';
+import {UsersPropsType} from './UsersContainer';
+import userPhoto from '../../assets/images/icon.png'
 
 const Users = (props: UsersPropsType) => {
-   if(props.users.length === 0 ) {
-       props.setUsers([
-           {
-               id: v1(),
-               photoURL: 'https://fotki.ykt.ru/albums/userpics/23871/803469.jpg',
-               followed: false,
-               fullName: "Dima",
-               status: 'I am boss',
-               location: {city: 'Minsk', country: 'Belarus'}
-           },
-           {
-               id: v1(),
-               photoURL: 'https://i.pinimg.com/originals/3f/f1/20/3ff120824122616c4922797749f58fba.jpg',
-               followed: true,
-               fullName: "Max",
-               status: 'I am boss',
-               location: {city: 'Moscow', country: 'Russia'}
-           },
-           {
-               id: v1(),
-               photoURL: 'https://pictureholiday.ru/wp-content/uploads/2018/05/24-10-800x551-min.jpg',
-               followed: false,
-               fullName: "Zak",
-               status: 'I am boss',
-               location: {city: 'Polotsk', country: 'Belarus'}
-           },
-           {
-               id: v1(),
-               photoURL: 'https://bipbap.ru/wp-content/uploads/2018/01/ec6fb8fc51e8ea719853fcf3e0c71449e7083c73r1-1272-1920v2_uhq.jpg',
-               followed: true,
-               fullName: "Pol",
-               status: 'I am boss',
-               location: {city: 'Kiev', country: 'Ukraine'}
-           },
-       ],)
-   }
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        })
+    }
 
     return <div>{
         props.users.map((u: any) => {
             return <div key={u.id}>
                   <span>
                       <div className={s.item}>
-                            <img src={u.photoURL}/>
+                            <img src={u.photos.small ? u.photos.small : userPhoto}/>
                       </div>
                       <div>
                           <button onClick={() => props.toggleFollow(u.id)}>{u.followed ? 'Unfollow' : 'Follow'}</button>
@@ -54,12 +25,12 @@ const Users = (props: UsersPropsType) => {
                   </span>
                 <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                             <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                             <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
                         </span>
                     </span>
             </div>
