84806702e7e7800874371fcb566f67b3eaee3b4a
DimaWeb44 <dima.web44@gmail.com>
2022-02-10 21:09:25 +0300

    lesson 54

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -7,11 +7,9 @@ import {UsersPropsType} from './UsersContainer';
 
 class Users extends Component <UsersPropsType> {
 
-    constructor(props: UsersPropsType) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            this.props.setUsers(response.data.items)
-        })
+            this.props.setUsers(response.data.items)})
     }
 
     render(): ReactNode {
